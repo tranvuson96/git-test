ef77255d26bc624dc5b621f2f546f1c3ea22db6a
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-11-30 18:51:20 +0700

    reupdate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props){
+    super(props)
 
+    this.state={
+      dishes:DISHES
+    }
+  }
   render() {
     return (
       <div className="App">
-          <Menu />
+          <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
