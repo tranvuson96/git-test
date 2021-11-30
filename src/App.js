40901f5e1a66913f6c5472735eef29dd8b4cc4a0
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-11-30 18:41:46 +0700

    reupdate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Menu from './components/MenuComponent';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+          <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
