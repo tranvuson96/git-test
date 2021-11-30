42544c2f65c95dd687fbef0c1bc1425a8ef16561
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-11-30 20:15:05 +0700

    reupdate

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,14 +2,9 @@ import React from 'react';
 import {Card,CardBody,CardText,CardTitle,CardImg} from 'reactstrap';
 import dateFormat from 'dateformat';
 
-class Details extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        if(this.props.dish!=null)
-        {const comments=this.props.dish.comments.map((comment)=>{
+function Details(props){
+        if(props.dish!=null)
+        {const comments=props.dish.comments.map((comment)=>{
             return (
                     <div key={comment.id}>
                         <p>-- {comment.author}, {dateFormat(comment.date,"dd/mm/yyyy")}</p>
@@ -22,17 +17,17 @@ class Details extends React.Component{
             <div className="row">
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                    <CardImg top src={props.dish.image} alt={props.dish.name} />
                     <CardBody>
-                      <CardTitle>{this.props.dish.name}</CardTitle>
-                      <CardText>{this.props.dish.description}</CardText>
+                      <CardTitle>{props.dish.name}</CardTitle>
+                      <CardText>{props.dish.description}</CardText>
                     </CardBody>
                 </Card>
             </div>
             <div className="col-12 col-md-5 m-1">
             <Card>
                 <CardBody>
-                    <CardTitle>{this.props.dish.name}</CardTitle>
+                    <CardTitle>{props.dish.name}</CardTitle>
                     <CardText>Imagine all the eatables,living in conFusion!</CardText>
                     {comments}
                 </CardBody>
@@ -42,7 +37,7 @@ class Details extends React.Component{
         </div>
         )}
         else {return (<div></div>)}
-    }
+    
 }
 
 export default Details;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+import { Card, CardImg, CardImgOverlay,
     CardTitle } from 'reactstrap';
-import Details from './DishDetailComponent';
 
-class Menu extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                    onClick={()=>this.props.onClick(dish.id)}
-                >
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
-
+function RenderMenu({dish,onClick}){
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
-            </div>
+            <Card key={dish.id}
+                onClick={()=>onClick(dish.id)}
+            >
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardImgOverlay>
+                  <CardTitle>{dish.name}</CardTitle>
+              </CardImgOverlay>
+            </Card>
         );
-    }
+    
+}
+
+const Menu=(props)=>{
+    const menuItems=props.dishes.map((dish)=>{
+        return (
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <RenderMenu dish={dish} onClick={props.onClick} />
+            </div>
+        )
+    })
+    return (
+        <div className="container">
+            <div className="row">
+                {menuItems}
+            </div>
+        </div>
+    )
 }
 
 export default Menu;
