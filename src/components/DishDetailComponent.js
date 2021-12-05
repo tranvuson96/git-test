78c550764c5d7323965b50178b871d00176ac45c
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-12-05 17:02:33 +0700

    fetch from server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -18,6 +18,7 @@ import dateFormat from "dateformat";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import Loading from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -53,7 +54,7 @@ function RenderComments({ comments }) {
 function RenderDish({ dish }) {
 	return (
 		<Card>
-			<CardImg width='100%' src={dish.image} alt={dish.name} />
+			<CardImg top src={baseUrl + dish.image} alt={dish.name} />
 			<CardBody>
 				<CardTitle>{dish.name}</CardTitle>
 				<CardText>{dish.description}</CardText>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,8 +8,13 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { addComment, fetchDishes } from "../redux/actionCreators";
 import { actions } from "react-redux-form";
+import {
+	addComment,
+	fetchDishes,
+	fetchComments,
+	fetchPromos,
+} from "../redux/actionCreators";
 
 const mapPropsToState = (state) => {
 	return {
@@ -29,6 +34,8 @@ const mapDispatchToProps = (dispatch) => ({
 	resetFeedbackForm: () => {
 		dispatch(actions.reset("feedback"));
 	},
+	fetchComments: () => dispatch(fetchComments()),
+	fetchPromos: () => dispatch(fetchPromos()),
 });
 
 class Main extends React.Component {
@@ -40,6 +47,8 @@ class Main extends React.Component {
 
 	componentDidMount() {
 		this.props.fetchDishes();
+		this.props.fetchComments();
+		this.props.fetchPromos();
 	}
 
 	render() {
@@ -53,12 +62,13 @@ class Main extends React.Component {
 								(dish) => dish.id === parseInt(match.params.dishId, 10),
 							)[0]
 						}
-						comments={this.props.comments.filter(
-							(comment) => comment.dishId === parseInt(match.params.dishId, 10),
-						)}
-						addComment={this.props.addComment}
 						isLoading={this.props.dishes.isLoading}
 						errMess={this.props.dishes.errMess}
+						comments={this.props.comments.comments.filter(
+							(comment) => comment.dishId === parseInt(match.params.dishId, 10),
+						)}
+						commentsErrMess={this.props.comments.errMess}
+						addComment={this.props.addComment}
 					/>
 				</React.Fragment>
 			);
@@ -69,7 +79,13 @@ class Main extends React.Component {
 					dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
 					dishesLoading={this.props.dishes.isLoading}
 					dishesErrMess={this.props.dishes.errMess}
-					promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+					promotion={
+						this.props.promotions.promotions.filter(
+							(promo) => promo.featured,
+						)[0]
+					}
+					promoLoading={this.props.promotions.isLoading}
+					promoErrMess={this.props.promotions.errMess}
 					leader={this.props.leaders.filter((leader) => leader.featured)[0]}
 				/>
 			);
