0c22ed03d4e389390c4ce1c1fc5ca952df197fe8
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-12-12 16:20:21 +0700

    react component part 1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+import Main from "./Components/Main";
+class App extends Component {
+	render() {
+		return <Main />;
+	}
+}
+
+export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Menu from "./Menu";
+export class Main extends Component {
+	render() {
+		return (
+			<div>
+				<Menu />
+			</div>
+		);
+	}
+}
+
+export default Main;
