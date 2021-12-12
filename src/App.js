ad230c181650549e2744925398b4ebcdd2bdd392
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-12-12 16:24:58 +0700

    react  component part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./Components/Main";
+import { DISHES } from "./share/dishes";
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			dishes: DISHES,
+		};
+	}
 	render() {
-		return <Main />;
+		return <Main dishes={this.state.dishes} />;
 	}
 }
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Menu from "./Menu";
 export class Main extends Component {
+	constructor(props) {
+		super(props);
+	}
 	render() {
 		return (
 			<div>
-				<Menu />
+				<Menu dishes={this.props.dishes} />
 			</div>
 		);
 	}
