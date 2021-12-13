9eaedf503a1449200494c7f996b186c051dbc3bc
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-12-13 15:56:53 +0700

    building and deploying

---
+++ check.js
@@ -0,0 +1,24 @@
+const array = [
+	{
+		name: son,
+		mod: admin,
+	},
+	{
+		name: t,
+		mod: t,
+	},
+	{
+		name: t,
+		mod: t,
+	},
+	{
+		name: t,
+		mod: t,
+	},
+	{
+		name: t,
+		mod: t,
+	},
+];
+
+console.log(array.findIndex(t));
