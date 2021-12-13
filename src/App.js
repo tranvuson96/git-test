cac08398938e83138217db212e2f4ab2fd1df41c
Tran Vu Son <sontvFX11243@fuxni.edu.vn>
2021-12-13 20:37:49 +0700

    config react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
-class App extends Component {
+import React, { Component } from "react";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
 
-  render() {
-    return (
-      <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
-      </div>
-    );
-  }
+class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<Navbar dark color='primary'>
+					<div className='container'>
+						<NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+					</div>
+				</Navbar>
+			</div>
+		);
+	}
 }
 
 export default App;
